62d8af95a2d390bb9d563cbb771eea970591fef2
mini <mini@minimatoMac-mini.local>
2012-04-09 17:45:27 +0800

    Initial Commit

---
+++ JingYingCar/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  JingYingCar
+//
+//  Created by JiaMing Yan on 12-4-9.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
